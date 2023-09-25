84e76b068dca6b531bf348e0f30ef3fd41296bdf
William Allen <wallentx@users.noreply.github.com>
2023-09-25 11:24:32 -0500

    Update build_linux_flatpak-installer.sh

--- build_scripts/build_linux_flatpak-installer.sh
+++ build_scripts/build_linux_flatpak-installer.sh
@@ -75,8 +75,8 @@ if [ "$PLATFORM" = "arm64" ]; then
   sudo gem install fpm
   sudo apt -y install flatpak flatpak-builder
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak install flathub org.freedesktop.Sdk//22.08
-  flatpak install flathub org.freedesktop.Platform//22.08
+  flatpak install -y --noninteractive flathub org.freedesktop.Sdk//22.08
+  flatpak install -y --noninteractive flathub org.freedesktop.Platform//22.08
   echo USE_SYSTEM_FPM=true env DEBUG="@malept/flatpak-bundler" npx electron-builder build --linux flatpak --arm64 \
     --config.linux.desktop.Name="Chia Blockchain" \
     --config.artifactName="chia-blockchain"
@@ -87,8 +87,8 @@ if [ "$PLATFORM" = "arm64" ]; then
 else
   sudo apt -y install flatpak flatpak-builder
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak install flathub org.freedesktop.Sdk//22.08
-  flatpak install flathub org.freedesktop.Platform//22.08
+  flatpak install -y --noninteractive flathub org.freedesktop.Sdk//22.08
+  flatpak install -y --noninteractive flathub org.freedesktop.Platform//22.08
   echo DEBUG="@malept/flatpak-bundler" npx electron-builder build --linux flatpak --x64 \
     --config.linux.desktop.Name="Chia Blockchain" \
     --config.artifactName="chia-blockchain"
